99141dbcd3e5a8c88fd884060a597ac4dfd5788c
Owais <precisework01@gmail.com>
2021-04-16 04:30:51 +0500

    updated

---
+++ src/apollo/client.js
@@ -0,0 +1,13 @@
+import fetch from 'cross-fetch';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+
+export const client = new ApolloClient({
+  link: new HttpLink({
+    uri: '/.netlify/functions/mybookmarkList',
+    fetch,
+  }),
+  cache: new InMemoryCache()
+});
+
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,105 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React, { useEffect, useState } from "react"
+import { useQuery, useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import "./styles.css";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import BookmarksIcon from '@material-ui/icons/Bookmarks';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
 
-export default IndexPage
+
+
+
+
+
+const GET_BOOKMARKS = gql`
+{
+    bookmarks {
+        id
+        url
+        title
+    }
+}
+`;
+
+const ADD_BOOKMARK = gql`
+    mutation addBookmar($url: String!, $title: String!){
+        addBookmark(url: $url, title: $title){
+            id
+        }
+    }
+`
+
+export default function Home() {
+
+    let titleField;
+    let urlField;
+
+    const { error, loading, data } = useQuery(GET_BOOKMARKS);
+    const [addBookmark] = useMutation(ADD_BOOKMARK);
+    const handleSubmit = () => {
+        console.log(titleField.value)
+        console.log(urlField.value)
+        addBookmark({
+            variables: {
+                url: urlField.value,
+                title: titleField.value
+            },
+            refetchQueries: [{ query: GET_BOOKMARKS }]
+        })
+    }
+
+    if (error)
+        return <h3>{error}</h3>
+
+    if (loading)
+        return <CircularProgress/>
+
+    return <div className="container">
+
+
+
+        <h2>Add New Bookmark</h2>
+        <label>
+            Enter Bookmark Tite: <br />
+            <input type="text" ref={node => titleField = node} />
+        </label>
+
+        <br />
+        <label>
+            Enter Bookmark Url: <br />
+            <input type="text" ref={node => urlField = node} />
+        </label>
+
+        <br />
+        <br />
+        <button onClick={handleSubmit}>Add Bookmark</button>
+
+        <h2>My Bookmark List</h2>
+
+
+        {data.bookmarks.map((item) => (
+           
+        <List key={item.id}>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <BookmarksIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={item.title} secondary={item.url} />
+        </ListItem>
+    
+      </List>
+          ))}
+        
+
+
+
+    </div>
+}
